dc96a167ed4dc265d31fd9a23d863d2e44226226
Tamim Shahriar <tash3080@gmail.com>
2024-09-14 23:23:04 +1000

    docker-compose-working

--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://tamim_shahriar_sit722_part3_user:xazxuJ6QgPO51OIBRRbxy4VjFGBG1aVW@dpg-criminu8ii6s73f66bdg-a.oregon-postgres.render.com/tamim_shahriar_sit722_part3" # "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
